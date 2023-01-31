4d4fb546efd207d3355b81ca14853ec240618657
Krystof Rainisch <krystof.rainisch@hey.com>
2023-01-31 22:31:36 +0100

    git add .

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# game-module
+
+## 0.1.0
+
+### Minor Changes
+
+- 6c2f6ec: change exports
---
+++ index.ts
@@ -0,0 +1,6 @@
+import Generator from './src/Generator'
+import {Quad,Triangle,Vertex} from './src/Generator'
+
+
+export default Generator
+export { Generator, Quad, Triangle, Vertex }
